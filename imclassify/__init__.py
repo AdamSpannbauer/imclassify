9cdc934ae403c9458dde3e06a19ea00771021f66
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-10-26 12:03:40 -0400

    add outline for main class

--- imclassify/__init__.py
+++ imclassify/__init__.py
@@ -1,3 +1,4 @@
 from .version import __version__
+from .im_classifier import ImClassifier
 
 __author__ = "Adam Spannbauer <spannbaueradam@gmail.com>"
---
+++ imclassify/im_classifier.py
@@ -0,0 +1,18 @@
+class ImClassifier:
+    def __init__(self):
+        pass
+
+    def gather_images(self):
+        pass
+
+    def extract_features(self):
+        pass
+
+    def train_model(self):
+        pass
+
+    def classify_image(self):
+        pass
+
+    def classify_video(self):
+        pass
